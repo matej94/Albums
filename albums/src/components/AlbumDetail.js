1be1253d7651b72d71eb946edcfa590ea1889e64
matej94 <matej.tomic123@gmail.com>
2018-10-24 17:26:18 +0200

    Fetched albums data from API, created AlbumList and AlbumDetail components and displayed data

---
+++ albums/src/components/AlbumDetail.js
@@ -0,0 +1,16 @@
+// Import libraries for making a component
+import React from 'react';
+import { Text, View } from 'react-native';
+
+// Make a component
+const AlbumDetail = (props) => {
+    return (
+        <View>
+            <Text>{props.album.title}</Text>
+        </View>
+    );
+};
+
+
+// Make the component available to other parts of the app
+export default AlbumDetail;
---
+++ albums/src/components/AlbumList.js
@@ -0,0 +1,33 @@
+// Import libraries for making a component
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import axios from 'axios';
+import AlbumDetail from './AlbumDetail';
+
+// Make a component
+class AlbumList extends Component {
+    
+    state = { albums: [] };
+
+    componentWillMount() {
+        axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+        .then(response => this.setState({ albums: response.data }));
+    }
+
+    renderAlbums() {
+        return this.state.albums.map(album => 
+        <AlbumDetail key={album.title} album={album} />
+        );
+    }
+
+    render() {
+        return (
+            <View>
+                {this.renderAlbums()}
+            </View>
+        );
+    }
+}
+
+// Make the component available to other parts of the app
+export default AlbumList;
